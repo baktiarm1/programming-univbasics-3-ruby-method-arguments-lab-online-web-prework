5728c57815e1630b2f88144f45d14f2a8021f34b
Baktiar Miah <baktiar.m@gmail.com>
2019-07-27 15:47:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,20 @@
+
+def introduction(name)
+   puts "Hi, my name is #{name}"
+end
+
+def introduction_with_language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+
+
 def introduction_with_language_optional (name, language='Ruby')
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-introduction('Bak', 'Ruby')
+
+
+introduction
+introduction_with_language
+introduction_with_language_optional
